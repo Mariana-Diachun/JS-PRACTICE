1a2ad26ae78cf32604955c7ae2f3cd1dfcc77406
Alla-Goptsii <goptsy.a@gmail.com>
2022-08-18 19:20:53 +0300

    add viewing

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-import "./js/references.js"
-import "./database/data.js"
-import "./js/header.js"
-import "./js/hero.js"
-import "./js/gallery.js"
-
+import "./js/references.js";
+import "./database/data.js";
+import "./js/header.js";
+import "./js/hero.js";
+import "./js/gallery.js";
+import "./js/viewing.js";
--- js/references.js
+++ js/references.js
@@ -1,8 +1,9 @@
 const references = {
-    header: document.querySelector('header'),
-    navigation: document.getElementById('navigation'),
-    hero: document.querySelector(`.hero`),
-    gallery: document.querySelector('.gallery'),
-}
+  header: document.querySelector("header"),
+  navigation: document.getElementById("navigation"),
+  hero: document.querySelector(`.hero`),
+  gallery: document.querySelector(".gallery"),
+  viewing: document.querySelector(".viewing"),
+};
 
 export default references;
