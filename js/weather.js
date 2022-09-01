709365e34faab5ffc2cb8f0123db917aef8b3ff5
Alla-Goptsii <goptsy.a@gmail.com>
2022-09-01 23:52:05 +0300

    add createMarkup

--- js/weather.js
+++ js/weather.js
@@ -1,18 +1,13 @@
 import refs from "./references.js";
 
 const API_KEY = "64d6b90eedb4f846285071799db733c3";
-const endpointGeo = "/geo/1.0/direct";
-const endpointData = "/data/2.5/weather";
-// const countryCode = 'UA';
-// const url = BASE_URL + endpointGeo + queryParams;
 
-// https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
 axios.defaults.baseURL = "https://api.openweathermap.org";
 console.dir(axios);
 class Weather {
   constructor() {
-    this.endpointGeo = endpointGeo;
-    this.endpointData = endpointData;
+    this.endpointGeo = "/geo/1.0/direct";
+    this.endpointData = "/data/2.5/weather";
     this._cityName = "Kyiv";
     // this.geoLocation = {
     //   lon: 30.5241361,
@@ -43,39 +38,18 @@ class Weather {
     axios
       .get(url)
       .then((response) => {
-        console.log(response);
         if (response.status === 200) {
           return response.data;
         } else {
           throw new Error(response.statusText);
         }
       })
-      .then((data) => {
-        console.log(data);
-        return data[0];
-      })
+      .then((data) => data[0])
       .then((city) => {
-        // console.log(city.lat, city.lon);
         this.geoLocation = { lat: city.lat, lon: city.lon };
-        // console.log(this.geoLocation);
         this.getDataByGeo();
       })
       .catch((error) => alert(error));
-    // fetch(url)
-    //   .then((response) => {
-    //     if (response.ok) {
-    //       return response.json();
-    //     }
-    //     // console.log(response);
-    //   })
-    //   .then((data) => {
-    //     return data[0];
-    //     // console.log(data);
-    //   })
-    //   .then((city) => {
-    //     console.log(city);
-    //   })
-    //   .catch();
   }
 
   getDataByGeo() {
@@ -84,27 +58,75 @@ class Weather {
     axios
       .get(url)
       .then((response) => {
-        console.log(response);
         if (response.status !== 200) {
           throw new Error(response.statusText);
         }
         return response.data;
       })
       .then((data) => {
-        console.log(data);
-        const temp = (data.main.temp - 273.15).toFixed(2);
-        console.log("temp:", `${temp}°C`);
-        const pressure = data.main.pressure;
-        console.log("pressure:", `${pressure}mm`);
-        const humidity = data.main.humidity;
-        console.log("humidity:", `${humidity}%`);
-        const weatherData = data.weather.map((item) => {
-          console.log(item);
-        });
+        const widgetMarkup = createWidgetPreview(data);
+        refs.weather.insertAdjacentHTML("afterbegin", widgetMarkup);
+        refs.weather.addEventListener("click", this.openFullWidget);
       });
+  }
+
+  openFullWidget() {
+    //
   }
 }
 
 const weather = new Weather();
 console.log(weather);
 weather.getGeoByCityName();
+
+function createMarkup(data) {
+  const cityLocation = data.name;
+  const temp = (data.main.temp - 273.15).toFixed(2);
+  const humidity = data.main.humidity;
+  const windSpeed = data.wind.speed;
+  const icons = createWeatherIcon(data.weather).join("");
+  //  const pressure = data.main.pressure;
+  // console.log("pressure:", `${pressure}mm`);
+  return `<h2 class="city">
+      Weather in ${cityLocation}
+    </h2>
+    <h1 class="temp">
+      ${temp}°C
+    </h1>
+    <ul class="flex">
+      ${icons}
+    </ul>
+    <div class="humidity">
+      Humidity: ${humidity}%
+    </div>
+    <div class="wind">
+      Wind speed: ${windSpeed}m/s
+    </div>`;
+}
+
+function createWeatherIcon(icons) {
+  return icons.map((icon) => {
+    const desc = icon.description;
+    const iconPath = `https://openweathermap.org/img/wn/${icon.icon}.png`;
+    return `<li>
+              <img src=${iconPath} alt=${desc} class="icon" />
+              <div class="description">
+              ${desc}
+              </div>
+          </li>`;
+  });
+}
+
+function createWidgetPreview(data) {
+  const icons = createWeatherIcon(data.weather).join("");
+  const temp = (data.main.temp - 273.15).toFixed(2);
+
+  return `
+   <h1 class="temp">
+      ${temp}°C
+    </h1>
+     <ul class="flex">
+      ${icons}
+    </ul>
+  `;
+}
